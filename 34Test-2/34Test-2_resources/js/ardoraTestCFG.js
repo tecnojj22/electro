0b93d15da4eeb1b4c986584330a1e867577f9341
tecnojj22 <jorgejurado1822@gmail.com>
2025-12-16 20:27:24 +0100

    Add files via upload

---
+++ 34Test-2/34Test-2_resources/js/ardoraTestCFG.js
@@ -0,0 +1,41 @@
+//Creado con Ardora - www.webardora.net
+//bajo licencia Attribution-NonCommercial-NoDerivatives 4.0 International (CC BY-NC-ND 4.0)
+//para otros usos contacte con el autor
+var timeAct=360; timeIni=360; timeBon=0;
+var successes=0; successesMax=1; attempts=0; attemptsMax=1;
+var score=0; scoreMax=1; scoreInc=1; scoreDec=1
+var typeGame=0;
+var tiTime=false;
+var tiTimeType=0;
+var tiButtonTime=true;
+var textButtonTime="Comenzar";
+var tiSuccesses=false;
+var tiAttempts=false;
+var tiScore=false;
+var startTime;
+var colorBack="#FFFDFD"; colorButton="#91962F"; colorText="#000000"; colorSele="#FF8000";
+var goURLNext=false; goURLRepeat=false;tiAval=false;
+var scoOk=0; scoWrong=0; scoOkDo=0; scoWrongDo=0; scoMessage=""; scoPtos=10;
+var fMenssage="Verdana, Geneva, sans-serif";
+var fActi="Verdana, Geneva, sans-serif";
+var fResp="Verdana, Geneva, sans-serif";
+var fEnun="Verdana, Geneva, sans-serif";
+var timeOnMessage=5; messageOk="MUY BIEN"; messageTime=""; messageError="ERROR"; messageErrorG="ERROR"; messageAttempts=""; isShowMessage=false;
+var urlOk=""; urlTime=""; urlError=""; urlAttempts="";
+var goURLOk="_blank"; goURLTime="_blank"; goURLAttempts="_blank"; goURLError="_blank"; 
+borderOk="#008000"; borderTime="#FF0000";borderError="#FF0000"; borderAttempts="#FF0000";
+var wordsGame="MzRUZXN0LTI="; wordsStr = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/=";
+function giveZindex(typeElement){var valueZindex=0; capas=document.getElementsByTagName(typeElement);
+for (i=0;i<capas.length;i++){if (parseInt($(capas[i]).css("z-index"),10)>valueZindex){valueZindex=parseInt($(capas[i]).css("z-index"),10);}}return valueZindex;}
+var tags=["Determina la intensida de la corriente eléctrica a través de una resistencia de 40Ω al aplicarle una diferencia de potencial de 90V."];
+var answers1=["MTEsNSBB","MDMsMzAgQQ==","MDAsNDUgQQ==","MDIsNzYgQQ=="];
+var ans=[answers1];
+var err=[""];
+var ima=["Captura_de_pantalla_2025-12-15_182332.png"];
+var mp4=[""];
+var ogv=[""];
+var alt=[""];
+var info=[""];
+var indexTag=0; actualAnswers=[]; dirMedia="34Test-2_resources/media/";
+var tiRandOrder=true;
+var iT=0;var r_order=[];
